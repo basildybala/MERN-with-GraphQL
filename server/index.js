00a7ae14e8b4dcef93267b3b1b43e44f5467ce93
basildybala <akbasil123@gmail.com>
2025-07-05 09:44:53 +0530

    Grqaph ql code added

--- server/index.js
+++ server/index.js
@@ -1,9 +1,25 @@
 let express =require('express')
 require('dotenv').config()
-
+const { graphqlHTTP } = require('express-graphql');
+const schema =require('./schema/schema')
+const connectDB =require('./config/db')
+let colors =require('colors')
 let port=process.env.PORT
 
+
+//connect to database
+connectDB()
+
 let app =express()
+
+
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema,
+    graphiql: process.env.NODE_ENV === 'devolopment',
+  })
+);
 
 
 app.listen(port,console.log(`Server is running on ${port}`))
---
+++ server/schema/schema.js
@@ -0,0 +1,56 @@
+const {
+  GraphQLObjectType,
+  GraphQLID,
+  GraphQLString,
+  GraphQLSchema,
+  GraphQLList,
+  GraphQLNonNull,
+  GraphQLEnumType,
+} = require('graphql');
+
+const {projects,clients}=require('../sampleData') 
+
+// Project Type
+const ProjectType = new GraphQLObjectType({
+  name: 'Project',
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    description: { type: GraphQLString },
+    status: { type: GraphQLString },
+  }),
+});
+
+// Client Type
+const ClientType = new GraphQLObjectType({
+  name: 'Client',
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    email: { type: GraphQLString },
+    phone: { type: GraphQLString },
+  }),
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    projects: {
+      type: new GraphQLList(ProjectType),
+      resolve(parent, args) {
+        return projects;
+      },
+    },
+    project: {
+      type: ProjectType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        return projects.find((project) => project.id === args.id);
+      },
+    }
+  },
+}); 
+
+module.exports = new GraphQLSchema({
+  query: RootQuery
+});
